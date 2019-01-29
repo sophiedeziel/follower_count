1b2c4a94e0142a85f06d0893bab51b07d0473094
Sophie Déziel <courrier@sophiedeziel.com>
2019-01-28 21:53:38 -0500

    Fetch le user ID avec l'API de Twitch

--- followercount.ino
+++ followercount.ino
@@ -1,9 +1,59 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
+
+
+HTTPClient http;
+const String twitch_name = "sophiedeziel";
+const String twitch_thumbprint = "0819d6edfd12c734212d56383e1428e2cefd61ea";
+
+int streamer_id;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    Serial.println("Retrying connection…");
+    delay(5);
+  }
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  streamer_id = getStreamerId(twitch_name);
+  Serial.println(streamer_id);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+
+  delay(30000);
+
+  // https://api.twitch.tv/helix/users?login=<username>
+
+  //"/users/follows?to_id="
+
+}
+
+int getStreamerId(String username) {
+  http.begin("https://api.twitch.tv/helix/users?login=sophiedeziel", twitch_thumbprint);
+
+  http.addHeader("Client-ID", twitch_client);
+  int httpCode = http.GET();
+  String payload = http.getString();
+  http.end();
+ 
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(payload);
+
+  int id = root["data"][0]["id"];
+  return id;
 }
